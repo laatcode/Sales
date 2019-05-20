1bb6d9256299d3c83a4fb5ab50f7250554d507a9
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2019-05-20 18:42:55 -0500

    Instalación y configuración de phroute a través de composer

--- public/index.php
+++ public/index.php
@@ -1,4 +1,21 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
+use Phroute\Phroute\RouteCollector;
+
 $route = $_GET['route'] ?? '/';
-echo $route;
+$router = new RouteCollector();
+
+$router->get('/', function(){
+  return 'Root';
+});
+
+try {
+  $dispatcher = new Phroute\Phroute\Dispatcher($router->getData());
+  $response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], $route);
+  echo $response;
+} catch(Phroute\Phroute\Exception\HttpRouteNotFoundException $e) {
+  echo "Ruta no encontrada";
+} catch (Phroute\Phroute\Exception\HttpMethodNotAllowedException $e) {
+  echo "Método no encontrado";
+}
  ?>
